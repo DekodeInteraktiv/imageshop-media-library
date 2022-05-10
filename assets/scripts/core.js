51f5ff46e235a312312d59a8dd55bce9a223296f
Marius Jensen <marius@clorith.net>
2022-05-10 11:20:21 +0200

    Update settings to not rely on jQuery

--- assets/scripts/core.js
+++ assets/scripts/core.js
@@ -1,31 +1,22 @@
-function send_request(data) {
-	jQuery.ajax({
-		type: 'POST',
-		url: ajaxurl,
-		data: data,
-		dataType: 'html'
-	}).done(function (res) {
-		isml_message.show()
-		isml_message.html('<br/>' + res)
-		isml_loader.hide()
-		jQuery('html,body').animate({scrollTop: 0}, 1000)
-	})
-}
-
-const ismlLoadingIndicator = document.getElementsByClassName( '.isml__loader' )[0],
-	ismlMessage = document.getElementsByClassName( '.isml__message' )[0],
-	ismlTestConnection = document.getElementsByClassName( '.isml__test__connection' )[0],
-	ismlSyncToImageshop = document.getElementsByClassName( '.isml__sync_wp_to_imageshop' )[0],
-	ismlSyncToLocal = document.getElementsByClassName( '.isml__sync_imageshop_to_wp' )[0];
+const ismlLoadingIndicator = document.getElementsByClassName( 'isml__loader' )[0],
+	ismlMessage = document.getElementsByClassName( 'isml__message' )[0],
+	ismlTestConnection = document.getElementsByClassName( 'isml__test__connection' )[0],
+	ismlSyncToImageshop = document.getElementsByClassName( 'isml__sync_wp_to_imageshop' )[0],
+	ismlSyncToLocal = document.getElementsByClassName( 'isml__sync_imageshop_to_wp' )[0];
 
 // check connection button
-ismlTestConnection.on('click', function () {
-	send_request({
-			isml_api_key: document.querySelector('input[name=isml_api_key]').value,
-			action: 'isml_test_connection'
+ismlTestConnection.addEventListener( 'click', function () {
+	wp.apiFetch( {
+		path: '/imageshop/v1/settings/test-connection',
+		method: 'POST',
+		data: {
+			token: document.querySelector( 'input[name=isml_api_key]' ).value
 		}
-	)
-})
+	} )
+		.then( function( response ) {
+			ismlMessage.innerHTML = response.message;
+		} );
+} );
 
 ismlSyncToImageshop.addEventListener( 'click', function() {
 	ismlLoadingIndicator.style.display = 'block';
@@ -47,13 +38,4 @@ ismlSyncToLocal.addEventListener( 'click', function() {
 
 			ismlMessage.innerHTML = response.data.message;
 		} );
-} );
-
-ismlSyncToImageshop.on('click', function () {
-	send_request({action: 'isml_import_wp_to_imageshop'})
-} );
-
-ismlSyncToLocal.on('click', function () {
-	console.log('intrta')
-	send_request({action: 'isml_import_imageshop_to_wp'})
 } );
--- includes/class-helpers.php
+++ includes/class-helpers.php
@@ -10,8 +10,7 @@ class Helpers {
 	private static $instance;
 
 	public function __construct() {
-		add_action( 'wp_ajax_isml_test_connection', array( $this, 'test_connection' ) );
-
+		add_action( 'rest_api_init', array( $this, 'register_rest_routes' ) );
 	}
 
 	/**
@@ -26,47 +25,56 @@ class Helpers {
 		return self::$instance;
 	}
 
-	/**
-	 *
-	 */
-	public function test_connection() {
-		if ( 'POST' === $_SERVER['REQUEST_METHOD'] ) {
-			$api_key = $_POST['isml_api_key'] ?? '';
-		}
-		try {
-			$isml_rest_controller = new REST_Controller( $api_key );
-			$can_upload           = $isml_rest_controller->can_upload();
-			if ( $can_upload ) {
-				$this->show_message( __( 'Connection is successfully established. Save the settings.', 'imageshop' ) );
-			} else {
-				$this->show_message( __( 'Connection Error.', 'imageshop' ), true );
-			}
-
-			exit();
-		} catch ( \Exception $e ) {
-			$this->show_message(
-				__(
-					'Connection is not established.',
-					'imageshop'
-				) . ' : ' . $e->getMessage() . ( 0 === $e->getCode() ? '' : ' - ' . $e->getCode() ),
-				true
-			);
-			exit();
-		}
+	public function register_rest_routes() {
+		register_rest_route(
+			'imageshop/v1',
+			'/settings/test-connection',
+			array(
+				'methods'             => \WP_REST_Server::CREATABLE,
+				'callback'            => array( $this, 'test_connection' ),
+				'permission_callback' => function() {
+					return current_user_can( 'manage_options' );
+				}
+			)
+		);
 	}
 
 	/**
-	 * @param       $message
-	 * @param false $errormsg
+	 *
 	 */
-	public function show_message( $message, $errormsg = false ) {
-		if ( $errormsg ) {
-			echo '<div id="message" class="error">';
-		} else {
-			echo '<div id="message" class="updated fade">';
+	public function test_connection( \WP_REST_Request $request ) {
+		$api_key = $request->get_param( 'token' );
+
+		$response = array(
+			'message' => sprintf(
+				'<div class="notice notice-success fade"><p>%s</p></div>',
+				esc_html__( 'Connection is successfully established. Save the settings.', 'imageshop' )
+			)
+		);
+
+		try {
+			$isml_rest_controller = new REST_Controller( $api_key );
+			$can_upload           = $isml_rest_controller->can_upload();
+
+			if ( ! $can_upload ) {
+				$response['message'] = sprintf(
+					'<div class="notice notice-warning"><p>%s</p></div>',
+					esc_html__( 'Could not establish a connection to Imageshop.', 'imageshop' )
+				);
+			}
+		} catch ( \Exception $e ) {
+			$response['message'] = sprintf(
+				'<div class="notice notice-error"><p>%s</p></div>',
+				sprintf(
+					// translators: 1: Error message. 2: Error code.
+					esc_html__( 'Could not establish a connection: %1$s (%2$d)', 'imageshop' ),
+					$e->getMessage(),
+					$e->getCode()
+				)
+			);
 		}
 
-		echo "<p><strong>$message</strong></p></div>";
+		return new \WP_REST_Response( $response );
 	}
 
 	/**
